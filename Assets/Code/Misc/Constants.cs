8918030a66d402eeb7fb637b7d48ce739badb518
Harald <harald921@hotmail.com>
2018-06-09 23:31:14 +0200

    Create simple NetworkManager

--- Assets/Code/Misc/Constants.cs
+++ Assets/Code/Misc/Constants.cs
@@ -6,6 +6,7 @@ using System.IO;
 public static class Constants
 {
     public static readonly string APP_NAME = "Cubic";
+    public static readonly string GAME_VERSION = "0.01";
 
     public static string TILEMAP_SAVE_FOLDER => Path.Combine(Application.dataPath + "/../", "Maps");
 
