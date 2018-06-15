3b9f576e58c1d98775103ecaf15df69a0dc9956a
Harald <harald921@hotmail.com>
2018-06-15 23:47:14 +0200

    Add files needed for turning Character into a MonoBehaviour

--- Assets/Code/CharacterCommander.cs
+++ Assets/Code/CharacterCommander.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class CharacterCommander : Photon.MonoBehaviour
 {
+    public void ServerMovePlayer()
+    {
+        // Check if can move
+        // If true, begin moving player
+        // Tell the other clients to move the player
 
+        // If not, tell the player who asked this to undo
+    }
+
+    public void LocalMovePlayer()
+    {
+        // Check if can move
+        // If true, begin moving the player locally
+        // Tell the server to move the player the same way
+    }
 }
