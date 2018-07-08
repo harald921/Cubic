8569671fc1927fa54d747e3842728ebda2e37f68
Cylec <simeliaz@hotmail.com>
2018-07-09 01:12:24 +0200

    Update desert level

---
+++ Assets/Code/Misc/DecalRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecalRotation : MonoBehaviour
+{
+    [SerializeField] float _rotationSpeed = 90;
+    
+    void Start()
+    {
+        transform.Rotate(0, Random.Range(0,360), 0);
+    }
+
+	void Update ()
+    {
+        transform.Rotate(Vector3.up * (_rotationSpeed * Time.deltaTime));
+    }
+}
