1cfe5289048281cb1e4347726756365bfc4cffb9
Johan-1 <nilssonjohan10@gmail.com>
2018-07-10 20:44:53 +0200

    start work on placeholder friends matchmaking

--- Assets/Code/Menus/MainMenu.cs
+++ Assets/Code/Menus/MainMenu.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-	// ALL CODE HERE IS TEMP FOR THE MOMENT, JUST TO TRY TO SYNC LEVEL LOADING ON ALL PEOPLE IN A ROOM FROM MENU SCENE
 	// THIS WILL BE THE MASTER CLASS TO HANDLE ALL THE DIFFERENT MENU PAGES IN THE GAME
 	[SerializeField] bool _debugMessages = false;
 
@@ -15,28 +14,8 @@ public class MainMenu : MonoBehaviour
 
 		PhotonNetwork.automaticallySyncScene = true;
 
-		ConnectToServer();
-	}
-
-	void ConnectToServer() =>
 		PhotonNetwork.ConnectUsingSettings(Constants.GAME_VERSION);
 
-	void OnConnectedToMaster() =>
-		JoinRoom();
-
-	void JoinRoom() =>
-		PhotonNetwork.JoinOrCreateRoom("debugRoomName", new RoomOptions(), TypedLobby.Default);
-
-	void OnCreatedRoom()
-	{
-		if (_debugMessages) Debug.Log("New room created");
-	}
-
-	void OnJoinedRoom()
-	{
-		if (_debugMessages) Debug.Log("Connected to room");
-
-		Debug.LogFormat("Num players connected to room {0}", PhotonNetwork.playerList.Length);
 	}
 
 	void Update()
---
+++ Assets/Code/Menus/PlayWithFriendsPage.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon;
+
+public class PlayWithFriendsPage : Photon.MonoBehaviour
+{
+
+	[SerializeField] GameObject _content;
+	[SerializeField] GameObject _levelScreen;
+
+	[SerializeField] Text _roomNameText;
+	[SerializeField] Text _joinRoomInput;
+	[SerializeField] Text _numJoinedPlayersText;
+
+	[SerializeField] Button _continueButton;
+
+
+	void Awake()
+	{
+		_continueButton.interactable = false;	
+	}
+
+	public void HostRoom()
+	{
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.IsVisible = false;
+		roomOptions.MaxPlayers = 4;
+
+		// generate a random name, if we let photon create a name for us its about 100 characters long, works untill we intergrate steam
+		string roomName = Random.Range(1000, 50000).ToString();
+
+		PhotonNetwork.CreateRoom(roomName, roomOptions, TypedLobby.Default);
+	}
+
+	public void JoinRoom()
+	{
+		if (!PhotonNetwork.isMasterClient)
+		   PhotonNetwork.JoinRoom(_joinRoomInput.text);
+	}
+
+	public void GoToLevelSelect()
+	{
+		PhotonNetwork.room.IsOpen = false;
+		photonView.RPC("ContinueToLevelselect", PhotonTargets.All);
+	}
+
+	void OnCreatedRoom()
+	{
+		_roomNameText.text = PhotonNetwork.room.Name + " As Host";
+	}
+
+	void OnJoinedRoom()
+	{
+		if (!PhotonNetwork.isMasterClient)
+		   _roomNameText.text = PhotonNetwork.room.Name + " As Client";
+		
+		photonView.RPC("UpdateNumPlayersInRoom", PhotonTargets.All, PhotonNetwork.room.PlayerCount);
+	}
+
+	void OnPhotonPlayerDisconnected(PhotonPlayer otherPlayer)
+	{
+		photonView.RPC("UpdateNumPlayersInRoom", PhotonTargets.All, PhotonNetwork.room.PlayerCount);
+	}
+
+	[PunRPC]
+	void UpdateNumPlayersInRoom(int numPlayers)
+	{
+		_numJoinedPlayersText.text = string.Format("{0}/4", PhotonNetwork.room.PlayerCount.ToString());
+
+		if (PhotonNetwork.isMasterClient && PhotonNetwork.room.PlayerCount > 1)
+			_continueButton.interactable = true;
+	}
+
+	[PunRPC]
+	void ContinueToLevelselect()
+	{
+		_levelScreen.SetActive(true);
+		_content.SetActive(false);
+	}
+
+
+
+	
+
+}
